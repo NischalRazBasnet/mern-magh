8678cf56b312f847cb3f8d598d1c5bbcd87a9b14
Nischal <nischalbasnet7848@gmail.com>
2025-03-01 22:53:53 +0545

    1st js

--- dom.js
+++
@@ -1,7 +0,0 @@
-const doc = document.getElementById("hi");
-
-console.log(doc);
-
-const change = () => {
-  doc.style.color = "red";
-};
---
+++ index.js
@@ -0,0 +1,19 @@
+let firstName = 'Nischal';
+let lastName = 'Basnet';
+const age = 22;
+let address = 'phutung,kathmandu';
+let Qualfication = 'Bachelors';
+const isCoding = true;
+
+console.log(firstName.charCodeAt(0));
+console.log(firstName.concat('Hello'));
+console.log(firstName.charAt(0));
+console.log(firstName.includes('Ni'));
+console.log(firstName.includes('ni'));
+console.log(firstName.length);
+console.log(firstName.trim().length);
+console.log(firstName.slice(4));
+console.log(firstName.slice(0, 4));
+console.log(firstName.repeat(3));
+console.log(firstName.substring(3));
+console.log(firstName.substring(0, 6));
